410fa551962640224eecb32f2f48153b404289e5
avnit07 <avnitsingh58@gmail.com>
2026-02-09 23:38:06 +0530

    feat: integrate auth state with header and footer

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,18 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    status : false,
-    userData: null
+    status : false,     // user is not logged in
+    userData: null       // no user information
 }
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
+        // login reducer
         login: (state,action) => {
             state.status = true;
             state.userData = action.payload.userData;
         },
+        // logout reducer
         logout: (state) => {
             state.status = false;
             state.userData = null;
